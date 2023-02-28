970ef193f07402365795d26fc1bb16a5f6c62d8a
DzawilUqul <112853502+DzawilUqul@users.noreply.github.com>
2023-02-28 13:50:31 +0700

    Initialize

---
+++ dzawil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Pemrograman PHP</h1>
+
+<?php
+    $nama = "Dzawil";
+    echo "Selamat datang di pemrograman PHP : $nama"
+?>
+
+<?php
+    $nilai1 = 15;
+    $nilai2 = 40;
+    $total = $nilai1 + $nilai2;
+
+    echo "<h3>$nilai1 + $nilai2 = $total</h3>"
+?>
+    
+</body>
+</html>
+
